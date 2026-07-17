70155c86e9d8618ea0f9f86d1bc049f117946dae
Ayesha Asad <ayeshaxasadd@gmail.com>
2026-07-17 15:11:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+const countriesDiv = document.getElementById("countries");
+const searchInput = document.getElementById("search");
+const loading = document.getElementById("loading");
+const error = document.getElementById("error");
+
+let countries = [];
+
+fetchCountries();
+
+function fetchCountries() {
+
+    loading.textContent = "Loading countries...";
+
+    fetch("https://restcountries.com/v3.1/all?fields=name,flags,population")
+
+        .then(response =>
+         response.json()
+
+        )
+
+        .then(data => {
+
+            countries = data;
+
+            loading.textContent = "";
+
+            displayCountries(countries);
+
+        })
+
+        .catch(err => {
+
+            loading.textContent = "";
+            console.log(err);
+            error.textContent = "Failed to load data.";
+
+        });
+}
+
+function displayCountries(countryList) {
+
+    countriesDiv.innerHTML = "";
+
+    countryList.forEach(country => {
+
+        countriesDiv.innerHTML += `
+            <div class="card">
+                <img src="${country.flags.png}">
+                <h3>${country.name.common}</h3>
+                <p>Population: ${country.population}</p>
+            </div>
+        `;
+    });
+}
+
+searchInput.addEventListener("input", () => {
+
+    const searchText = searchInput.value.toLowerCase();
+
+    const filteredCountries = countries.filter(country =>
+        country.name.common.toLowerCase().includes(searchText)
+    );
+
+    displayCountries(filteredCountries);
+});
