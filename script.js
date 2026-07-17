e3b0417d0e1aefc497b76905c512e522cac04ff2
Ayesha Asad <ayeshaxasadd@gmail.com>
2026-07-17 15:52:40 +0500

    Refactor country fetching and filtering logic

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-const countriesDiv = document.getElementById("countries");
-const searchInput = document.getElementById("search");
-const loading = document.getElementById("loading");
-const error = document.getElementById("error");
-
-let countries = [];
-
-fetchCountries();
-
-function fetchCountries() {
-
-    loading.textContent = "Loading countries...";
-
-    fetch("https://restcountries.com/v3.1/all")
-
-        .then(response =>
-         response.json()
-
-        )
+     )
 
         .then(data => {
 
@@ -26,36 +8,8 @@ function fetchCountries() {
 
             displayCountries(countries);
 
-        })
-
-        .catch(err => {
-
-            loading.textContent = "";
-            console.log(err);
-            error.textContent = "Failed to load data.";
-
-        });
-}
-
-function displayCountries(countryList) {
-
-    countriesDiv.innerHTML = "";
-
-    countryList.forEach(country => {
-
-        countriesDiv.innerHTML += `
-            <div class="card">
-                <img src="${country.flags.png}">
-                <h3>${country.name.common}</h3>
-                <p>Population: ${country.population}</p>
-            </div>
-        `;
-    });
-}
-
-searchInput.addEventListener("input", () => {
-
-    const searchText = searchInput.value.toLowerCase();
+      
+   
 
     const filteredCountries = countries.filter(country =>
         country.name.common.toLowerCase().includes(searchText)
@@ -63,3 +17,4 @@ searchInput.addEventListener("input", () => {
 
     displayCountries(filteredCountries);
 });
+   
